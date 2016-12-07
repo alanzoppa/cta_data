6f02eb6a3a62c46c9d5c20fdac6e07bcdaf7cf23
C. Alan Zoppa <czoppa@enova.com>
2016-12-07 11:27:30 -0600

    better radial query that uses the index

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -3,8 +3,25 @@ class Stop < ApplicationRecord
   has_and_belongs_to_many :routes
 
   def self.where_near(longitude, latitude, radius) # radius in meters
-    Stop.where(
-      "earth_distance(ll_to_earth(longitude, latitude), ll_to_earth(#{longitude}, #{latitude})) < #{radius}"
+
+    # earth_box uses the indices, but earth_distance gives the real answer
+    # earth_box is just constraining the set that earth_distance operates
+    # against
+
+    Stop.includes(:street, :routes).where(
+      <<-SQL
+      earth_box(
+        ll_to_earth(longitude, latitude),
+        #{radius}
+      ) @> ll_to_earth(#{longitude}, #{latitude})
+
+      AND
+      
+      earth_distance(
+        ll_to_earth(longitude, latitude),
+        ll_to_earth(#{longitude}, #{latitude})
+      ) < #{radius}
+      SQL
     )
 
   end
