01d52858f92610c4d8a8321f68ae26912a74a227
C. Alan Zoppa <alan.zoppa@gmail.com>
2016-12-11 17:29:43 -0600

    some aggregation routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,5 +1,17 @@
 class RoutesController < ApplicationController
-  def show
-    params[:id]
+  def longest_by_stops
+    routes = Route.select("*,(SELECT count(route_id) from routes_stops WHERE routes_stops.route_id = routes.id) as stop_count").order('stop_count DESC')
+    render json: {route: routes}
+  end
+
+  def longest_by_distance
+    routes = Route.includes(:stops).map do |r|
+      any_stop_on_route = r.stops.first
+      {
+        distance: r.farthest_stops_from(any_stop_on_route)[:distance_between],
+        route: r
+      }
+    end.sort_by {|s| -s[:distance]}
+    render json: {route: routes}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/stops/route/:route_name', to: 'stops#stops_for_route'
   get '/stops', to: 'stops#index'
   get '/stops/:id', to: 'stops#show'
+  get '/routes/longest_by_stops', to: 'routes#longest_by_stops'
+  get '/routes/longest_by_distance', to: 'routes#longest_by_distance'
   get '', to: 'stops#index'
 end
